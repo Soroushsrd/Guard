f9aca14e23eaa1e0557347797dc26cbd14c89fac
MisaghMomeniB <jenabemisagh@gmail.com>
2024-11-28 22:19:26 +0330

    Added Undo and Redo functionality, Added file save and load functionality, Improved content editing capabilities, Enhanced user input handling, Improved cursor management

--- src/editor.rs
+++ src/editor.rs
@@ -1,11 +1,15 @@
 use crate::terminal::{Position, Terminal};
 use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers, read};
 use std::io::{self, Write};
+use std::fs::{self, File};
+use std::io::Write as IoWrite;
 
 pub struct Editor {
     should_quit: bool,
     content: String,
     position: Position,
+    undo_stack: Vec<String>,
+    redo_stack: Vec<String>,
 }
 
 impl Editor {
@@ -14,6 +18,8 @@ impl Editor {
             should_quit: false,
             content: String::new(),
             position: Position::default(),
+            undo_stack: Vec::new(),
+            redo_stack: Vec::new(),
         }
     }
 
@@ -42,14 +48,17 @@ impl Editor {
                     self.should_quit = true;
                 }
                 KeyCode::Char(c) if *kind == KeyEventKind::Release => {
+                    self.save_undo_state();
                     self.content.push(*c);
                     self.position.x += 1;
                 }
                 KeyCode::Backspace if self.position.x > 0 => {
+                    self.save_undo_state();
                     self.content.pop();
                     self.position.x -= 1;
                 }
                 KeyCode::Enter => {
+                    self.save_undo_state();
                     self.content.push('\n');
                     self.position.y += 1;
                     self.position.x = 0;
@@ -66,9 +75,62 @@ impl Editor {
                 KeyCode::Down => {
                     self.position.y += 1;
                 }
+                KeyCode::F(1) => {
+                    self.save_to_file().unwrap();
+                }
+                KeyCode::F(2) => {
+                    self.load_from_file().unwrap();
+                }
+                KeyCode::Z if *modifiers == KeyModifiers::CONTROL => {
+                    self.undo();
+                }
+                KeyCode::Y if *modifiers == KeyModifiers::CONTROL => {
+                    self.redo();
+                }
                 _ => (),
             }
         }
+    }
+
+    pub fn save_undo_state(&mut self) {
+        self.undo_stack.push(self.content.clone());
+        if self.undo_stack.len() > 10 {
+            self.undo_stack.remove(0); // limit to 10 undo actions
+        }
+        self.redo_stack.clear(); // Clear redo stack when new action is performed
+    }
+
+    pub fn undo(&mut self) {
+        if let Some(last_state) = self.undo_stack.pop() {
+            self.redo_stack.push(self.content.clone());
+            self.content = last_state;
+            self.position.x = self.content.lines().last().map_or(0, |line| line.len());
+            self.position.y = self.content.lines().count() - 1;
+        }
+    }
+
+    pub fn redo(&mut self) {
+        if let Some(last_state) = self.redo_stack.pop() {
+            self.undo_stack.push(self.content.clone());
+            self.content = last_state;
+            self.position.x = self.content.lines().last().map_or(0, |line| line.len());
+            self.position.y = self.content.lines().count() - 1;
+        }
+    }
+
+    pub fn save_to_file(&self) -> Result<(), io::Error> {
+        let mut file = File::create("output.txt")?;
+        file.write_all(self.content.as_bytes())?;
+        Terminal::print("\nContent saved to 'output.txt'.\n")?;
+        Ok(())
+    }
+
+    pub fn load_from_file(&mut self) -> Result<(), io::Error> {
+        let content = fs::read_to_string("output.txt")?;
+        self.content = content;
+        self.position = Position { x: 0, y: 0 };
+        Terminal::print("\nContent loaded from 'output.txt'.\n")?;
+        Ok(())
     }
 
     pub fn repl(&mut self) -> Result<(), io::Error> {
